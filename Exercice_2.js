4f63d13e85101e891bf7cfb18ecb00e2132313e3
Hana-Houimli <hanahouimli003@gmail.com>
2025-09-28 11:15:12 +0200

    Exercice_2

---
+++ Exercice_2.js
@@ -0,0 +1,10 @@
+// Fonction classique
+function somme_1(a,b)
+{
+    return a + b;
+}
+console.log(somme_1(3,2));
+
+// Fonction fléchées
+const somme_2 = (x,y) => x + y;
+console.log(somme_2(3,2));
